a03a495fd4b05fc0b6b3bf621da5769a4a66da4e
aleksandrbarinov <barabarinov@gmail.com>
2022-06-15 13:46:39 +0300

    renamed file

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -7,7 +7,7 @@ from app.buttons import reply_keyboard_order
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_video(
-        video=open('pics/toptapki_4.mp4', 'rb'),
+        video=open('pics/toptapki.mp4', 'rb'),
         caption=hello_text,
         reply_markup=reply_keyboard_order(),
         parse_mode=ParseMode.MARKDOWN,
